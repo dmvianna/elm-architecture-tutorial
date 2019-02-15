1ae8db8e827b610f7e652b2b1d1c6f4d9efbf41c
Daniel Vianna <dmlvianna@gmail.com>
2019-02-15 17:59:09 +1100

    add age

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -20,35 +20,39 @@ type alias Model =
   { name : String
   , password : String
   , passwordAgain : String
+  , age : String
   }
 
 
 init : Model
 init =
-  Model "" "" ""
+  Model "" "" "" ""
 
 
 
 -- UPDATE
 
-
 type Msg
   = Name String
   | Password String
   | PasswordAgain String
+  | Age String
 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Name name ->
-      { model | name = name }
+        { model | name = name }
 
     Password password ->
-      { model | password = password }
+        { model | password = password }
 
     PasswordAgain password ->
-      { model | passwordAgain = password }
+        { model | passwordAgain = password }
+
+    Age age ->
+        { model | age = age }
 
 
 
@@ -57,14 +61,15 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ viewInput "text" "Name" model.name Name
-    , viewInput "password" "Password" model.password Password
-    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
-    , viewValidation model
-    ]
+    div []
+        [ viewInput "text" "Name" model.name Name
+        , viewInput "password" "Password" model.password Password
+        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , viewInput "age" "Age" model.age Age
+        , viewValidation model
+        ]
 
-
+        
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
 viewInput t p v toMsg =
   input [ type_ t, placeholder p, value v, onInput toMsg ] []
